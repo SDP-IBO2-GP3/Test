e6657ecf6d7c27ada09b10860ca7047a065c3b5d
Corentin Lemaitre <corentinlemaitre1996@gmail.com>
2017-09-29 12:59:28 +0200

    first commit test

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args){
+        System.out.println("Hello Wolrd ! ");
+        LOGGER.error("Hello world logger ");
+    }
+}
